20ed1763d6458632f53f044507a277746f1d7b63
MitchellXOTWOD <slimmitch.mn@gmail.com>
2023-09-25 19:10:18 -0500

    fixed rounding bug

--- app/page.jsx
+++ app/page.jsx
@@ -51,7 +51,7 @@ const Home = ({selectedWeapon, setSelectedItem}) => {
           <button onClick={() => setRefreshes((x) => x+1)} className="nice-button md:w-1/6 self-center text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
             Roll Weapon
           </button>
-          <p>rolled {refreshes} time(s) with a {odds.toFixed(3)}% chance</p>
+          {odds && <p>rolled {refreshes} time(s) with a {odds.toFixed(3)}% chance</p>}
         </div>
       </div>}
     </div>
