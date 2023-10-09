e44e5346dc98e9882daf544aed8807fa07248dc3
MitchellXOTWOD <slimmitch.mn@gmail.com>
2023-10-09 15:31:07 -0500

    Number of perks selectable
    The perks are selectable, but the odds are not calculated accordingly. Will fix in a future update

--- components/roll list/single perk list/SinglePerkList.jsx
+++ components/roll list/single perk list/SinglePerkList.jsx
@@ -3,24 +3,51 @@ import { useEffect, useState } from 'react';
 
 const SinglePerkList = ({Perks, Type, isSmallScreen, refreshes}) => {
 
-  const [randomIndex, setRandomIndex] = useState(0);
+  const [randomIndex, setRandomIndex] = useState([]);
+  const [perkNum, setPerkNum] = useState(1);
   
   if(!Perks){
     return <p>Loading...</p>
   }
 
+  const handlePerkNumChange = (event) => {
+    let inputNum = event.target.value;
+    //don't allow negative numbers or out of bounds numbers
+    if(inputNum > 0 && inputNum <= Perks.length && inputNum != ''){
+      setPerkNum(inputNum);
+    }
+    else{
+      setPerkNum(1);
+    }
+  }
+
   useEffect(() => {
-    setRandomIndex(Math.floor(Math.random() * Perks.length));
-  }, [refreshes])
+    //randomIndexArray holds a dynamic amount of values that will be randomly selected as perks
+    let randomIndexArray = [];
+
+    //do not allow duplicate numbers in the array. If the number is already in the array, roll another number.
+    while (randomIndexArray.length < perkNum) {
+    let randomIndex = Math.floor(Math.random() * Perks.length);
+
+    // Check if the random index is already in the array
+    if (!randomIndexArray.includes(randomIndex)) {
+      randomIndexArray.push(randomIndex);
+    }
+    }
+    setRandomIndex(randomIndexArray);
+  }  
+  , [refreshes])
 
   return (
     <div className='h-full bg-slate-800 flex flex-col items-center'>
       <h2 className='mb-5'>{Type}</h2>
+      <label htmlFor='perkNum'>Number of {Type}s:</label>
+      <input type='text' id='perkNum' name='perkNum' placeholder={"select a number"} onChange={handlePerkNumChange} className='text-black'/><br/>
       <ul>
       {/* do not include trait locked perks */}
       {Perks.map((perk, index) => (
         perk.name !== "Trait Locked" ? (
-        <li key={index} className={`mb-3 flex items-center ${index === randomIndex ? 'bg-yellow-500' : ''}`}>
+        <li key={index} className={`mb-3 flex items-center ${randomIndex.includes(index) ? 'bg-yellow-500' : ''}`}>
           {perk.hasIcon && <Image src={perk.icon} height={50} width={50} alt="weapon-screenshot" className="" />}
           {!isSmallScreen && perk.name}
         </li>
